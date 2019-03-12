c6387ea00f2299a9e8cac729a9726a14c0e9b1f8
KimReady <kjb4380@gmail.com>
2019-03-12 22:21:56 +0900

    두 개 폼을 위한 URL 적용, views와 tests에 있는 코드 이동, 새로운 URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     # Examples;
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    re_path(r'^lists/new$', views.new_list, name='new_list'),
 
     # path(r'^admin/', include(admin.site.urls))
 ]
